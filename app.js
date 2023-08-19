2140adacce51eb94ea1002b6dcae8bab845a8dac
Jojo <davidson.jojo@yahoo.com>
2023-08-19 17:23:35 -0500

    Made corrections

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/**
+============================================
+; Title: app.js
+; Author: John Davidson
+; Date: 08/19/2023
+; Description: The JavaScript file for WEB 420 - Assignment 1.2.
+============================================
+*/
+
 // Enables strict mode
 "use strict";
 
@@ -8,14 +17,17 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const mongoose = require('mongoose');
 
+// Creates an express application instance.
 const app = express();
 
 // Defines the port number
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json()); // Parse incoming JSON data.
+app.use(express.urlencoded({ extended: true })); // Parse incoming URL-encoded data. 
+
+// Configuration options for Swagger documentation configuration.
 const options = {
     definition: {
         openapi: '3.0.0',
@@ -27,10 +39,13 @@ const options = {
     apis: ['./routes/*.js'],
 };
 
+// Generates swagger specification. 
 const openapiSpecification = swaggerJsdoc(options);
 
+// Serve Swagger documentation using Swagger UI middleware.
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
+// Starts the express server and listens on port 3000. 
 app.listen(PORT, () => {
     console.log('Application started and listening on port ' + PORT)
 });
